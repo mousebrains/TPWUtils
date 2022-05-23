5412c019a80f0880b3baf397c22bb722a6fcafd3
Pat Welch <pat@mousebrains.com>
2022-05-23 19:17:03 +0000

    Change INotify.py to use pyinotify instead of inotify_simple, a bit of API change

--- INotify.py
+++ INotify.py
@@ -1,143 +1,106 @@
 #! /usr/bin/env python3
 #
-# Use inotify_simple to interface to the glibc version of inotify
-#
-# this handles setting up and taking down directory watches on the
-# full directory tree
+# Use pyinotify to handle file notification and send
+# the notification to a queue
 #
 
-import argparse
-from Thread import Thread
-import inotify_simple as ins
-import logging
-import os
-import time
+from argparse import ArgumentParser
+import pyinotify
 import queue
-import re
+import logging
+import time
+import os
+try:
+    from .Thread import Thread # As a module
+except:
+    from Thread import Thread # From within module
 
 class INotify(Thread):
-    def __init__(self, args:argparse.ArgumentParser):
+    def __init__(self, args:ArgumentParser, flags:int=None) -> None:
+        # queues:list[queue.Queue]=None, paths:list[str]=None,
+        # mask:int=pyinotify.ALL_EVENTS, qRecursive:bool=True, qAutoAdd:bool=True) -> None:
         Thread.__init__(self, "INotify", args)
+        self.__wm = pyinotify.WatchManager()
+        self.__notifier = pyinotify.Notifier(self.__wm)
         self.queue = queue.Queue()
-        self.__mapping = {}
-        self.__inotify = ins.INotify()
-        self.__flags = ins.flags.CREATE \
-                | ins.flags.MODIFY \
-                | ins.flags.CLOSE_WRITE \
-                | ins.flags.MOVED_TO \
-                | ins.flags.MOVED_FROM \
-                | ins.flags.MOVE_SELF \
-                | ins.flags.DELETE \
-                | ins.flags.DELETE_SELF
-    
-    def __repr__(self) -> str:
+        if flags is not None:
+            self.__flags = flags
+        else:
+            self.__flags = pyinotify.IN_CREATE 
+            self.__flags|= pyinotify.IN_MODIFY 
+            self.__flags|= pyinotify.IN_CLOSE_WRITE 
+            self.__flags|= pyinotify.IN_MOVED_TO 
+            self.__flags|= pyinotify.IN_MOVED_FROM
+            self.__flags|= pyinotify.IN_MOVE_SELF
+            self.__flags|= pyinotify.IN_DELETE
+            self.__flags|= pyinotify.IN_DELETE_SELF
+
+    @staticmethod
+    def __maskname(mask:int) -> str:
+        # This should be able to use pyinotify.EventsCodes.maskname, but it fails
         items = []
-        for key in self.__mapping:
-            items.append("{} -> {}".format(key, self.__mapping[key]))
-        return "\n".join(items)
+        codes = pyinotify.EventsCodes.FLAG_COLLECTIONS["OP_FLAGS"]
+        for key in codes:
+            if mask & codes[key]: items.append(key)
+        return "|".join(items) if items else None
 
-    def __addWatch(self, dirName:str) -> None:
-        wd = self.__inotify.add_watch(dirName, self.__flags)
-        self.__mapping[wd] = dirName
-        logging.debug("add %s %s", wd, dirName)
+    def addTree(self, tgt:str) -> None:
+        self.addWatch(tgt, qRecursive=True, qAutoAdd=True)
 
-    def __rmTree(self, root:str) -> None:
-        inotify = self.__inotify
-        toDrop = []
-        for key in self.__mapping:
-            if re.match(r"^" + root + r"(|/)$", self.__mapping[key]):
-                logging.debug("Removing %s %s", key, self.__mapping[key])
-                toDrop.append(key)
-                try:
-                    inotify.rm_watch(key)
-                except OSError: # No watched dir for the wd
-                    pass
-        if toDrop: # Separate loop so dictionary is not changing on us
-            for key in toDrop:
-                del self.__mapping[key]
-
-    def addTree(self, root:str) -> None:
-        inotify = self.__inotify
-        # Recursively walk through root's directory tree and add watches to each directory
-        for (dirpath, dirnames, filenames) in os.walk(root):
-            self.__addWatch(dirpath) # os.walk will hit all the dirnames too, so skip here
+    def addWatch(self, tgt:str, mask:int=None, qRecursive:bool=None, qAutoAdd:bool=None) -> bool:
+        tgt = os.path.abspath(os.path.expanduser(tgt))
+        if os.path.isdir(tgt):
+            mask = mask if mask is not None else self.__flags
+            qRecursive = qRecursive if qRecursive is not None else self.__qRecursive
+            qAutoAdd = qAutoAdd if qAutoAdd is not None else self.__qAutoAdd
+            self.__wm.add_watch(path=tgt, mask=mask, proc_fun=self.__eventHandler, 
+                    rec=qRecursive, auto_add=qAutoAdd)
+            logging.info("Added watch for %s, rec %s auto %s msk %s",
+                    tgt, qRecursive, qAutoAdd, self.__maskname(mask))
+            return True
+        logging.error("Path %s does not exist", tgt)
+        return False
 
     def runIt(self) -> None: # Called on thread start
-        inotify = self.__inotify
-        items = self.__mapping
-        q = self.queue
-        logging.info("Starting")
-        while True:
-            events = inotify.read()
-            t0 = time.time()
-            files = set()
-            toDelete = set()
-            toAdd = set()
-            try:
-                for event in events:
-                    wd = event.wd
-                    if (wd not in items):
-                        if not (event.mask & ins.flags.IGNORED):
-                            logging.warning("Missing wd for %s", event)
-                        continue
-                    path = items[wd] if event.name == "" else os.path.join(items[wd], event.name)
-                    files.add(path)
-                    logging.debug("event wd=%s name=%s %s %s", wd, event.name, path,
-                            ",".join(map(str, ins.flags.from_mask(event.mask))))
+        logging.warning("Starting loop")
+        self.__notifier.loop() # All the action happens in __eventHandler
+        logging.warning("Leaving loop")
 
-                    if event.mask & ins.flags.DELETE_SELF:
-                        toDelete.add(path)
-                    elif (event.mask & ins.flags.ISDIR) and (event.mask & ins.flags.CREATE):
-                        toAdd.add(path)
-                    elif (event.mask & ins.flags.ISDIR) and (event.mask & ins.flags.MOVED_FROM):
-                        toDelete.add(path)
-                    elif (event.mask & ins.flags.ISDIR) and (event.mask & ins.flags.MOVED_TO):
-                        toAdd.add(path)
-                    elif event.mask & ins.flags.MOVE_SELF:
-                        toDelete.add(path) # Delete myself and children
-                if files:
-                    q.put(("FILES", t0, files))
-
-                if toDelete:
-                    q.put(("DELETE", t0, toDelete))
-                    for item in toDelete:
-                        for item in toDelete:
-                            self.__rmTree(item)
-
-                if toAdd: 
-                    for item in toAdd:
-                        self.addTree(item)
-                    q.put(("ADD", t0, toAdd)) # Send after new watches are added
-            except:
-                logging.exception("GotMe")
+    def __eventHandler(self, e:pyinotify.Event) -> None:
+        t0 = time.time() # Time of the event
+        fn = e.path if e.dir else os.path.join(e.path, e.name)
+        self.queue.put((t0, fn))
+        logging.info("Event %s, %s", fn, e.maskname)
 
 if __name__ == "__main__":
-    from Logger import Logger
+    import Logger
 
-    parser = argparse.ArgumentParser()
+    class Reader(Thread):
+        def __init__(self, args:ArgumentParser, q:queue.Queue) -> None:
+            Thread.__init__(self, "Reader", args)
+            self.__queue = q
+
+        def runIt(self) -> None:
+            q = self.__queue
+            while True:
+                (t0, fn) = q.get()
+                q.task_done()
+                logging.info("%s %s", t0, fn)
+
+    parser = ArgumentParser()
     Logger.addArgs(parser)
-    parser.add_argument("dirs", nargs='+', type=str, help="directories to watch")
+    parser.add_argument("tgt", nargs="+", help="Directories to watch")
     args = parser.parse_args()
 
     Logger.mkLogger(args)
+
+    i = INotify(args)
+    rdr = Reader(args, i.queue)
+    i.start()
+    rdr.start()
+    for tgt in args.tgt: i.addTree(tgt)
+
     try:
-        inotify = INotify(args)
-
-        for item in args.dirs:
-            inotify.addTree(item)
-
-        inotify.start() # Start the thread
-        logging.info("isQueueEmpty %s", Thread.isQueueEmpty())
-        while Thread.isQueueEmpty():
-            logging.info("Looping")
-            try:
-                info = inotify.queue.get(timeout=60)
-                inotify.queue.task_done()
-                logging.info("info %s", info)
-            except queue.Empty:
-                pass
-
-        Thread.waitForException() # There should be something here
+        Thread.waitForException()
     except:
-        logging.exception("Unexpected exception")
-        
+        logging.exception("Exception from INotify")
