904006d3be46a4e4cc466dc97169d4a1cc38dce7
Pat Welch <mousebrains@users.noreply.github.com>
2023-06-04 09:00:22 -0700

    Pulled in from various other implementations

---
+++ Credentials.py
@@ -0,0 +1,32 @@
+#
+# load credentials
+#
+import os
+import logging
+import yaml
+
+def getCredentials(fn:str) -> str:
+    fn = os.path.abspath(os.path.expanduser(fn))
+    if os.path.isfile(fn):
+        try:
+            with open(fn, "r") as fp:
+                info = yaml.safe_load(fp)
+                if "username" in info and "password" in info:
+                    return (info["username"], info["password"])
+                logging.error("%s is not properly formated", fn)
+        except Exception as e:
+            logging.warning("Unable to open %s, %s", fn, str(e))
+
+    logging.info("Going to build a fresh AVISO credentials file, %s", fn)
+    info = {
+            "username": input("Enter username: "),
+            "password": input("Enter password: "),
+            }
+
+    if not os.path.isdir(os.path.dirname(fn)):
+        logging.info("Creating %s", os.path.dirname(fn)))
+        os.makedirs(os.path.dirname(fn), mode=0o700, exist_ok=True)
+
+    with open(fn, "w") as fp:
+        yaml.dump(info, fp, indent=4, sort_keys=True)
+    return (info["username"], info["password"])
